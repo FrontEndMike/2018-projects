e051b099044a6ff91dfcb65323a3f2f03c501853
Mike <michael.lenard.jackson@gmail.com>
2018-03-19 23:50:18 -0500

    remove unnecessary js

---
+++ portfolio-v1/js/scripts.js
@@ -0,0 +1,10 @@
+// yeah let's do vanilla JS just for fun :P
+
+// var menubtn = document.getElementById('menu-btn');
+// var slider = document.getElementById('slider');
+
+// menubtn.addEventListener('click', toggleSlider);
+
+// function toggleSlider(){
+// 	slider.classList.add("closed");
+// }
