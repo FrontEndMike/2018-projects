7f826593314de08e022cc78c6508398dcc0ddeac
Mike <michael.lenard.jackson@gmail.com>
2018-03-29 01:47:34 -0500

    live tweaks

--- portfolio-v1/contact.php
+++ portfolio-v1/contact.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'New Submission <michael.lenard.jackson@gmail.com>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = 'New Submission form <michael.lenard.jackson@gmail.com>';
 
 // subject of the email
 $subject = 'New message from contact form';
